43d9d83e3237f258d6a3ae2202ddf74c6f017f15
Aishwarya <harkareaishwarya@gmail.com>
2021-03-11 18:45:11 +0530

    added donor and payment structure

---
+++ contracts/donor.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.17;
+
+    struct Donator {
+
+        string name;
+        string message;
+        unit16 projectID;
+        uint value;
+        unit account_balance;
+
+    }
+
+    Donator[] public donators;  //stores data of all donators
+
+    contract make_donations{
+
+         Donator d;
+         function getProjects() public view returns (string [])
+       {
+          return CharityProjects;
+       }
+
+       function selectCharityProject (unit16 id) public{
+          Donator[projectID]= CharityProjects[id]
+       }
+
+
+
+
+
+
+    }
