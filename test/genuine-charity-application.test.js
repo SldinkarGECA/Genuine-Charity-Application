07b9b6fec1ff675869c15b1dc3edf8b9ebc15690
Shreyas-Penkar <shreyaspenkar999@gmail.com>
2021-05-20 12:54:38 +0530

    test file to test the functions

---
+++ test/genuine-charity-application.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const {interface,bytecode} = require("../compile");
+
+let accounts;
+let contract;
+
+beforeEach(async() => {
+  // get all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // deplaoy the contract using one accounts
+  contract = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data: bytecode })
+  .send({ from:accounts[0], gas:5000000 })
+})
+
+describe("genuine-charity-application",() => {
+  it("deploys a contract",() => {
+    console.log(contract);
+  });
+});
